968a49179093bde385d0124bfcd6d8cfe4a3ac75
Goreth Manuel <gorethmanuel0@gmail.com>
2024-02-14 18:33:00 +0100

    Add file vercel

--- db/config/config.js
+++ db/config/config.js
@@ -1,25 +1,39 @@
 require('dotenv').config();
+const pg = require("pg");
+
 
 module.exports = {
   "development": {
+    "dialect": process.env.DB_DIALECT,
+    "dialectModule": pg,
+    "host": process.env.DB_HOST,
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_BASE,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+    "define": {
+      timestamps: process.env.DB_TIMESTAMPS,
+    },
   },
   "test": {
+    "dialect": process.env.DB_DIALECT,
+    "dialectModule": pg,
+    "host": process.env.DB_HOST,
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_BASE,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+    "define": {
+      timestamps: process.env.DB_TIMESTAMPS,
+    },
   },
   "production": {
+    "dialect": process.env.DB_DIALECT,
+    "dialectModule": pg,
+    "host": process.env.DB_HOST,
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_BASE,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+    "define": {
+      timestamps: process.env.DB_TIMESTAMPS,
+    },
   }
 }
